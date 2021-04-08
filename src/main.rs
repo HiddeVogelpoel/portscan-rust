dcab5c4f8b90bafa0e977ad6bd483ddf86149c2a
Hidde Vogelpoel <hidde.vogelpoel@gmail.com>
2021-04-08 14:11:10 +0200

    Moved code around, fixed scans up to 65535

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
 use std::net::TcpStream;
 use std::env;
 
+// Function to store open ports on target
+static mut PORTS:Vec<u16> = vec![];
+
 fn main(){
 
     let args: Vec<String> = env::args().collect();
-
     let address = &args[1];
-    let mut ports = vec![];
+    
+    // Calling scanports function
+    scanports(address);
 
-    for i in 1..100{
+}
+
+fn scanports(ip: &str){
+    //Function is unsafe at the moment due to the PORTS variable
+    unsafe{
+        println!("Reached scanport with ip {}", ip);
+
+        //TODO: add different portscan modes (all, top 1000) https://nullsec.us/top-1-000-tcp-and-udp-ports-nmap-default/
+        //Loop to check all ports
+        for i in 1..=65535{
         
-        let ip = format!("{}:{}", address, i);
+            let ip = format!("{}:{}", ip, i);
         
-        if let Ok(_stream) = TcpStream::connect(ip) {
-            ports.push(i);
+            if let Ok(_stream) = TcpStream::connect(ip) {
+                PORTS.push(i);
+            }
         }
-    }
 
-    for i in ports.iter(){
-        println!("Port {} opened", i);
+        for i in PORTS.iter(){
+            println!("Port {} opened", i);
+        }
     }
 }
