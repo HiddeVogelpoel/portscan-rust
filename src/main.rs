d0a9655f7124a8b1ce34e5be4c3237321f841c2f
Hidde Vogelpoel <hidde.vogelpoel@gmail.com>
2021-04-08 14:30:26 +0200

    Added command line options

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 use std::net::TcpStream;
-use std::env;
+
+use clap::{Arg, App};
 
 // Function to store open ports on target
 static mut PORTS:Vec<u16> = vec![];
 
 fn main(){
 
-    let args: Vec<String> = env::args().collect();
-    let address = &args[1];
+    let matches = App::new("Portscanner")
+        .version("0.0.3")
+        .author("Hidde Vogelpoel")
+        .about("Basic portscanner")
+        .arg(Arg::with_name("target")
+            .short("t")
+            .long("target")
+            .takes_value(true)
+            .help("The target")
+            .default_value("127.0.0.1"))
+        .get_matches();
     
-    // Calling scanports function
-    scanports(address);
+    // Calling scanports function 
+    if let Some(t) = matches.value_of("target"){
+        scanports(t);
+    }
 
 }
 
